48050a86fa02575d527436b09333ed4205c52a32
Atharva Pingale <atharvapingale333@gmail.com>
2025-11-06 19:00:34 +0530

    Basic Setup done

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="py-40">
+      hello
+      <br/>
+        <Button>Click me</Button>
+    </div>
+  );
+}
---
+++ components/header.jsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+
+
+const header = () => {
+    return (
+        <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-10 supports-[backdrop-filter]:bg-background/60">
+            <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+                <Link href="/" className="flex items-center gap-2 cursor-pointer">
+                    <Image
+                        src="/logo-single.png"
+                        alt="Medimeet Logo"
+                        width={200}
+                        height={60}
+                        className="h-10 w-auto object-contain"
+                    />
+                </Link>
+
+                <div className="flex items-center space-x-2">
+                    <SignedOut>
+                        <SignInButton>
+                            <Button variant="secondary">Sign In</Button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn appearance = {{
+                        elements: {
+                            avatarBox: "w-8 h-8 rounded-full",
+                            userButtonPopoverCard: "shadow-md",
+                            userPreviewMainIdentifier: "font-medium"
+                        }
+                    }}>
+                        <UserButton />
+                    </SignedIn>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default header
