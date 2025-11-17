ae1147d04ec3626fadaaabebd85a0ac46b6bfccb
Atharva Pingale <atharvapingale333@gmail.com>
2025-11-17 20:32:46 +0530

    User credits configured

---
+++ app/(main)/doctors/[speciality]/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react'
+import { useParams } from 'next/navigation';
+
+const SpecialityPage = () => {
+
+    const {speciality} = useParams();
+
+  return (
+    <div>SpecialityPage: {speciality}</div>
+  )
+}
+
+export default SpecialityPage
